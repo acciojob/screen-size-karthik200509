803a88facdc78e15ca1f62f2d70dc5ae96d8d8c9
karthik200509 <karthikladdu800@gmail.com>
2026-06-02 12:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ const height=document.getElementById("height");
+        const width=document.getElementById("width");
+
+          height.innerText = window.innerHeight
+   width.innerText = window.innerWidth
+   window.addEventListener("resize" , function(){
+     height.innerText = window.innerHeight
+   width.innerText = window.innerWidth
+   })
